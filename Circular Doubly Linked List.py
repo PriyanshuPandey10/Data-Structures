504cce4ce0eb6f078729edbb6299fc8a17ec6a20
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-07-05 21:40:34 +0530

    Deletion

--- Circular Doubly Linked List.py
+++ Circular Doubly Linked List.py
@@ -75,6 +75,43 @@ class CDLL:
                 print(temp.item,end=' ')
                 temp=temp.next
 
+    #Define a method delete first() to delete first element from the list
+    def delete_first(self):
+        if self.start is not None:
+            if self.start.next==self.start:
+                self.start=None
+            else:
+                self.start.prev.next=self.start.next
+                self.start.next.prev=self.start.prev
+                self.start=self.start.next
+
+    #Define a method delete_last() to delete the last element from the list
+    def delete_last(self):
+        if self.start is not None:
+            if self.start.next==self.start:
+                self.start=None
+            else:
+                self.start.prev.prev.next=self.start
+                self.start.prev=self.start.prev.prev
+
+    #Define a method delete_item() to delete a specified item from the list
+    def delete_item(self,data):
+        if self.start is not None:
+            temp=self.start
+            if temp.item==data:
+                self.delete_first()
+            else:
+                temp=temp.next
+                while temp is not self.start:
+                    if temp.item==data:
+                        temp.next.prev=temp.prev
+                        temp.prev.next=temp.next
+                    temp=temp.next
+
+
+
+
+
 
 my_list=CDLL()
 my_list.insert_at_start(100)
@@ -87,6 +124,15 @@ my_list.insert_at_last(20)
 my_list.insert_at_last(30)
 my_list.insert_at_last(40)
 my_list.insert_after(my_list.search(20),25)
+my_list.print_list()
+print()
+my_list.delete_first()
+my_list.print_list()
+print()
+my_list.delete_last()
+my_list.print_list()
+print()
+my_list.delete_item(25)
 my_list.print_list()
 
 
