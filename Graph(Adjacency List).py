76ea52fb631c2387bafd00ccba085ebee06ed5bd
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-07-16 14:48:52 +0530

    Print adjacency list

--- Graph(Adjacency List).py
+++ Graph(Adjacency List).py
@@ -20,4 +20,6 @@ class Graph:
         else:
             print("Invalid Vertices")
             return False
-        
+    def print_adj_list(self):
+        for vertex,n in self.adj_list.items():
+            print("V",vertex,":",n)
